ec802561f3997f6e60edc4854d1f25da59e72b77
kipmaiyo <danielkipmaiyo44@gmail.com>
2023-07-19 17:54:40 +0300

    Assignment Done

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,16 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
+
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  
+  MesaVerde(); // => "Mesa Verde National Park"
+  console.log(RMFunctions.trees);// "Aspen and Pine"
+  console.log(RMFunctions.wildlife); // "Elk, Bighorn Sheep, Moose"
+  console.log(RMFunctions.elevator);//Attempted import error
 
   return <h1>Colorado State Parks!</h1>;
 }
